803b959f741e395a4a06ea6a6a5a9f346f7ec83e
mikezano <mikezanogit@gmail.com>
2014-06-03 16:15:10 -0700

    adding database creation file

---
+++ app/src/main/java/com/zano/asciitty/app/SQLite.java
@@ -0,0 +1,39 @@
+package com.zano.asciitty.app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by mamanzan on 6/3/2014.
+ */
+public class SQLite extends SQLiteOpenHelper {
+
+    public static final String TABLE_ASCII_ART = "ascii_art";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_DATA = "data";
+
+    private static final String DATABASE_NAME = "ascii_art.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_ASCII_ART + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_NAME + " text not null, "
+            + COLUMN_DATA + " text not null );";
+
+    public SQLite(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+}
